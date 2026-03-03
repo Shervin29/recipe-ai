518c0867de5b8716fe02b8dd1eaeadaf2ea5a299
Shervin Rumao <shervinrumao29@gmail.com>
2026-03-03 17:20:10 -0500

    Rotated API key for safety

--- back/server.js
+++ back/server.js
@@ -15,7 +15,7 @@ app.post("/generate", async (req, res) => {
 
   try {
     const response = await fetch(
-      "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent",
+      "https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent",
       {
         method: "POST",
         headers: {
